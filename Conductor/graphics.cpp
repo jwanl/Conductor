ebf2c097edfe36d7c96246dac19ba607b847f067
JV <juha.vanhala@netikka.fi>
2024-10-19 12:34:51 +0300

    Draw background

--- Conductor/graphics.cpp
+++ Conductor/graphics.cpp
@@ -2,13 +2,16 @@
 
 Model Graphics::m_track_plane;
 RenderTexture2D Graphics::m_track_plane_tex;
+Texture2D Graphics::m_background_tex;
 
-void Graphics::init()
+void Graphics::init(const char* backgroundTexture)
 {
 	Mesh planeMesh = GenMeshPlane(8.0f, 0.5f, 1, 1);
 	m_track_plane = LoadModelFromMesh(planeMesh);
 	m_track_plane_tex = LoadRenderTexture(512, 64);
 	SetMaterialTexture(m_track_plane.materials, 0, m_track_plane_tex.texture);
+
+	m_background_tex = LoadTexture(backgroundTexture);
 }
 
 RenderTexture2D& Graphics::getTrackRenderTexture()
@@ -19,7 +22,7 @@ RenderTexture2D& Graphics::getTrackRenderTexture()
 /* Draws to track render texture. Remember to change drawing mode first and after */
 void Graphics::drawRenderTexture(Track& track)
 {
-	ClearBackground(WHITE);
+	ClearBackground(ColorAlpha(WHITE, 0.0f));
 	
 	auto sub = track.getNextValues();
 	double leftSide = track.getTime();
@@ -36,4 +39,9 @@ void Graphics::renderTrack()
 	Vector3 size = { 1.0f, 1.0f, 1.0f };
 
 	DrawModelEx(m_track_plane, pos, rax, 185.539f, size, WHITE);
+}
+
+void Graphics::drawBackground()
+{
+	DrawTexture(m_background_tex, 0, 0, WHITE);
 }
--- Conductor/graphics.h
+++ Conductor/graphics.h
@@ -6,11 +6,13 @@
 
 class Graphics {
 public:
-	static void init();
+	static void init(const char* backgroundTexture);
 	static void drawRenderTexture(Track& track);
 	static void renderTrack();
 	static RenderTexture2D& getTrackRenderTexture();
+	static void drawBackground();
 private:
 	static Model m_track_plane;
 	static RenderTexture2D m_track_plane_tex;
+	static Texture2D m_background_tex;
 };
