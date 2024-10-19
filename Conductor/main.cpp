62452fce6f1fb51b83cd13e69150152ed68ce01e
Samuel Hughes <samuel.hughes@aalto.fi>
2024-10-19 17:07:32 +0300

    Main menu

--- Conductor/main.cpp
+++ Conductor/main.cpp
@@ -7,10 +7,15 @@
 #include <iostream>
 #include "level.h"
 
+enum class GameState {
+    MainMenu,
+    Level,
+    WinScreen
+};
 
 int main(void)
 {
-    
+    GameState state = GameState::MainMenu;
 
     // Initialization
     const int screenWidth = 1280;
@@ -24,14 +29,10 @@ int main(void)
     const char* music = "../resources/rwbk.wav";
     
 
-
-    
-    ToggleFullscreen();
-   
-   
+    //ToggleFullscreen();
   
 
-    Level* level = new Level(music);
+    Level* level = nullptr;
 
     
 
@@ -44,46 +45,77 @@ int main(void)
     Graphics::init("../resources/background.png", 
         {"../resources/conductor_0.png", "../resources/conductor_1.png", "../resources/conductor_2.png"});
 
-    level->play();
+    //level->play();
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //std::cout << GetFPS() << "\n";
-        // TODO: Update your variables here
+        switch (state) {
+        case GameState::MainMenu: {
+            BeginDrawing();
 
-        level->update();
+            Color col;
+            col.g = 255.0f * (std::sin(GetTime() * 2.3f) * 0.5f + 0.5f);
+            col.b = 255.0f * (std::sin(GetTime() * 1.8f) * 0.5f + 0.5f);
+            col.r = 255.0f * (std::sin(GetTime() * 1.31f) * 0.5f + 0.5f);
+            col.a = 255;
 
-        // Pre-draw: Render to texture
-        BeginTextureMode(Graphics::getTrackRenderTexture());
-        Graphics::drawRenderTexture(level->getTrack());
-        EndTextureMode();
+
+            ClearBackground(BLACK);
+            auto w = MeasureText("Super Conductor!", 128);
+            DrawText("Super Conductor!", GetScreenWidth() / 2.0f - w / 2.0f, 50, 128, col);
+
+
+            auto f = 64 + std::sin(GetTime() * 1.0f) * 1;
+            auto vec = MeasureTextEx(GetFontDefault(),"Press the only button to start!", f, 1.0f);
+            DrawTextEx(GetFontDefault(), "Press the only button to start!", { GetScreenWidth() / 2.0f - vec.x / 2.0f, 320.0f }, f, 1.0f, col);
+            //DrawText("Press the only button to start!", GetScreenWidth() / 2.0f - w / 2.0f, 320, f, WHITE);
+
+            if (IsKeyPressed(KEY_SPACE)) {
+                state = GameState::Level;
+                level = new Level(music);
+                level->play();
+            }
+
+            EndDrawing();
+        }break;
+        case GameState::Level: {
+            level->update();
+
+            // Pre-draw: Render to texture
+            BeginTextureMode(Graphics::getTrackRenderTexture());
+            Graphics::drawRenderTexture(level->getTrack());
+            EndTextureMode();
+
+
+            // Draw
+            BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            Graphics::drawBackground();
+
+            BeginMode3D(camera);
+            Graphics::renderTrack();
+            EndMode3D();
+
+            Graphics::drawConductor(*level);
+
+            const auto text_x = GetScreenWidth() * 0.6;
+            const auto text_y = GetScreenHeight() * 0.1;
+            DrawText(TextFormat("POINTS: %i", level->score()), text_x, text_y, 64,WHITE);
+            DrawFPS(100, 100);
+
+            EndDrawing();
+        }break;
+        case GameState::WinScreen: {
+
+        }break;
+        }
+
         
+
         
-
-
-       
-
-        // Draw
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        Graphics::drawBackground();
-
-        BeginMode3D(camera);
-        Graphics::renderTrack();
-        EndMode3D();
-
-        Graphics::drawConductor(*level);
-
-        const auto text_x = GetScreenWidth() * 0.6;
-        const auto text_y = GetScreenHeight() * 0.1;
-        DrawText(TextFormat("POINTS: %i", level.score()), text_x, text_y, 64, WHITE);
-        DrawFPS(100, 100);
-
-        EndDrawing();
     }
 
     // De-Initialization
