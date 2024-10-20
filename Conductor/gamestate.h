1598d0f514055cc9f295414a2cfdae8b856323cd
Samuel Hughes <samuel.hughes@aalto.fi>
2024-10-20 15:17:57 +0300

    bizet

--- Conductor/gamestate.h
+++ Conductor/gamestate.h
@@ -65,7 +65,7 @@ private:
     };
     const char* musics[3] = {
         RES("rwbk.mp3"),
-        RES("music.mp3"),
+        RES("bizet.mp3"),
         RES("wagner.mp3"),
     };
     float is_released = false;
