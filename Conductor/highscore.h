be474663a1d4ec129a28a27d917e9474a4ee0b7e
JV <juha.vanhala@netikka.fi>
2024-10-20 13:44:56 +0300

    Fixed high score

--- Conductor/highscore.h
+++ Conductor/highscore.h
@@ -4,23 +4,12 @@
 #include <fstream>
 #include <iostream>
 
-namespace HighScore {
+/* When this is ran for the first time, file is read */
+int getHighScore(int levelIndex);
 
-	const int amountOfLevels = 3; // TODO
-	const char* fileName = "sc.dat";
+/* Returns true if score was set (bigger than previous score) and false otherwise */
+bool setHighScore(int levelIndex, int score);
 
-	/* When this is ran for the first time, file is read */
-	int getHighScore(int levelIndex);
+void m_highscore_readFile();
 
-	/* Returns true if score was set (bigger than previous score) and false otherwise */
-	bool setHighScore(int levelIndex, int score);
-
-	void readFile();
-
-	void saveFile();
-
-	std::vector<int> m_scores;
-
-	bool m_read_already = false;
-
-}
+void m_highscore_saveFile();
